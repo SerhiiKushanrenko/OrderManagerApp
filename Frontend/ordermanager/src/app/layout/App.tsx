d492e3a4036743c110f90d20fa3db498402fbe2a
SerhiiKushanrenko <108808018+SerhiiKushanrenko@users.noreply.github.com>
2023-08-21 16:40:29 +0300

    setting up the layput

--- Frontend/ordermanager/src/app/layout/App.tsx
+++ Frontend/ordermanager/src/app/layout/App.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import './styles.css';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import React from 'react';
 import CustomersDashboard from '../../features/customers/customersDashboard/customersDashboard';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './styles.css';
+import Layout from './Layout';
+import HomePage from '../../features/home/HomePage';
 
 const client = new ApolloClient({
   cache: new InMemoryCache({
     typePolicies: {}
   }),
-  uri: "http://localhost:5257/graphql/"
-}); 
+  uri: process.env.REACT_APP_API_SCHEMA_URL
+});
 
-
-function App() {
+ export default function App() {
   return (
     <ApolloProvider client={client}>
-      <CustomersDashboard/>
+      <CustomersDashboard />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage/>} />
+            <Route path="customers" element={<CustomersDashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ApolloProvider>
   );
 }
-
-export default App;
